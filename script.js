3e83ba22232922f8966d6d6aade443fc75801c5d
haeril gunadi <144421938+haerilgun01@users.noreply.github.com>
2023-10-12 11:15:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let nama = prompt("masukkan kode");
+
+let tambah = prompt("masukkan 2 jika benar, masukkan 2 jika salah");
+
+let cek = confirm("apakah sudah benar");
+
+let hasil = tambah == "2" ? "benar" : "salah";
+
+alert(hasil);
